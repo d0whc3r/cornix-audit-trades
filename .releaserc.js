96e0b230747859edbf59eb4f63afee535443eb07
d0whc3r <d0whc3r@gmail.com>
2021-11-03 22:57:13 +0100

    fix: remove npm from semantic-release

--- .releaserc.js
+++ .releaserc.js
@@ -15,7 +15,6 @@ module.exports = {
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
-    '@semantic-release/npm',
     [
       '@semantic-release/github',
       {
